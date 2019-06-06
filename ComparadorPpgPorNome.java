ab9fbe8431bffefd8cd565cd5c6186299fc0114e
humbertogiuri <humbertogiuri@gmail.com>
2019-06-05 23:18:15 -0300

    Delete ComparadorPpgPorNome.java

--- ComparadorPpgPorNome.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ComparadorPpgPorNome implements Comparator<PPG> {
-	public int compare(PPG ppg, PPG outroPpg) {
-		return ppg.getNome().compareTo(outroPpg.getNome());
-	}
-}
